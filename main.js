5d40fccffe3ba61af517bab842b6fe5f088b4d1a
Abhimanyu Krishna <74185786+abhimanyu-ai@users.noreply.github.com>
2021-10-16 16:49:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+noseX=0;
+noseY=0;
 function preload() {
 
 }
@@ -7,7 +9,17 @@ function setup() {
   video=createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
+  poseNet=ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+  console.log("Posenet has been initialized");
+}
+function gotPoses(results) {
+  console.log(results);
+  noseX=results[0].pose.x.nose.x-15;
+  noseY=results[0].pose.y.nose.y-15;
 }
 function draw() {
-
+image(video, 0, 0, 300, 300);
 }
