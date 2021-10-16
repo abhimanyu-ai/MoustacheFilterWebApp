911a25fd6b939f64f424ba9c05f1ff9299ed2404
Abhimanyu Krishna <74185786+abhimanyu-ai@users.noreply.github.com>
2021-10-16 14:14:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 function preload() {
-    
-  }
-  
-  function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300, 300);
-    video.hide();
-  }
-  function draw() {
- 
-  }  
+
+}
+function setup() {
+  canvas=createCanvas(300, 300);
+  canvas.center();
+  video=createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+}
+function draw() {
+
+}
