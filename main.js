8c2827a7236a38ae68d7d6df2cee274279312ffa
Abhimanyu Krishna <74185786+abhimanyu-ai@users.noreply.github.com>
2021-10-18 11:46:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 noseX=0;
 noseY=0;
 function preload() {
-
+ moustache=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
+
 function setup() {
-  canvas=createCanvas(300, 300);
+  canvas = createCanvas(300, 300);
   canvas.center();
-  video=createCapture(VIDEO);
+  video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
-  poseNet=ml5.poseNet(video, modelLoaded);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
+
 function modelLoaded() {
-  console.log("Posenet has been initialized");
+  console.log('Posenet has been initialized');
 }
-function gotPoses(results) {
-  console.log(results);
-  noseX=results[0].pose.x.nose.x-15;
-  noseY=results[0].pose.y.nose.y-15;
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+  }
 }
+
 function draw() {
-image(video, 0, 0, 300, 300);
+  image(video, 0, 0, 300, 300);
+  image(moustache, noseX, noseY, 80, 35);
+}
+function take_snapshot(){    
+  save('My Moustche.png');
 }
